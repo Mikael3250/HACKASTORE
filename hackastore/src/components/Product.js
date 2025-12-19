7b37a5c811cb01371340b50bc1a71ccb234a5f5f
Mikael Bondan <mikaelbondan@gmail.com>
2025-12-19 20:36:15 -0300

    desenvolvendo sessão de produtos

--- hackastore/src/components/Product.js
+++ hackastore/src/components/Product.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import { faCartShopping, faMoneyBill } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Product() {
+export default function Product({ id, image, name, rate, price }) {
   return (
-    <div>Product</div>
-  )
+    <div className="product">
+      <img src={image} alt={name} />
+      <p className="name">{name}</p>
+      <p className="rate">&#9733; &#9733; &#9733; &#9733; </p>
+      <p className="price">
+        A partir de {price}
+        <span>R$</span>
+      </p>
+      <div className="buttons">
+        <Link to="/products/123/checkout" className="btn-icon">
+          <span>Contratar agora</span>
+          <FontAwesomeIcon icon={faMoneyBill} />
+        </Link>
+        <button className="btn-icon add-to-cart-btn">
+          <span>Adicionar ao carrinho</span>
+          <FontAwesomeIcon icon={faCartShopping} />
+        </button>
+      </div>
+    </div>
+  );
 }
